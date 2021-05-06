c2fdd8fafbe1f34cc76b912fc15915e32df2534f
David Mwendwa <klatzdavie19@gmail.com>
2021-05-06 16:13:12 +0300

    Built a small project to demonstate how higher order components work at a basic level.

---
+++ src/components/user-profile/user-profile.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import withData from '../../with-data';
+
+const UserProfile = ({ data, name, email }) => {
+  return (
+    <div className="container">
+      <h1>{name}</h1>
+      <h2>{email}</h2>
+      posts:
+      {data.map((post) => {
+        return (
+          <div className="post" key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default withData(UserProfile);
